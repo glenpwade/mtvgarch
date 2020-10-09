f69ea6aeffa9485282547efc56e456e2a7ec630e
AnniPunni <annastiina.silvennoinen@gmail.com>
2020-10-09 11:58:11 +1000

    added lag0 function (eats a vec and maxlag, returns a matrix whose cols = vec lagged by 1 to maxlag)
    added misspec test 3 from A&TT(2017)

--- R/misspec_dummy_code.R
+++ R/misspec_dummy_code.R
@@ -4,7 +4,6 @@ g <- # T x 1
 h <- # T x 1
 
 # derivatives:
-# (1/ht)*(dh/dtvpars)
 dg_dtv <- # T x nr.tv.pars
 dh_dtv <- # T x nr.tv.pars
 dh_dga <- # T x nr.garch.pars
@@ -50,5 +49,71 @@ resid = Y-X%*%t(b)   # T x 1
 SSR <- t(resid)%*%resid # 1x1
 # LM Robust
 LMrob <- Tobs-SSR
-prob <- (LMrob,df=NCOL(v3),lower.tail=FALSE)
+prob <- pchisq(LMrob,df=NCOL(v3),lower.tail=FALSE)
 }
+
+
+lag0(vX,lagrange){
+  ret <- matrix(0,nrow=NROW(vX),ncol=length(lagrange))
+  for (i in 1:length(lagrange)){
+    print(i)
+    lags[,i] <- c(0*c(1:lagrange[i]),vX[(1:(NROW(vX)-lagrange[i]))])
+  }
+  return(lags)
+}
+
+missTest3(one tvgarch obj at a time, maxlag){
+  #
+  g <- # T x 1
+  h <- # T x 1
+  z <- e/sqrt(g*h)  # T x 1
+  z2 <- z^2
+  z2_1 <- z^2-1
+
+  # derivatives:
+  dg_dtv <- # T x nr.tv.pars
+  dh_dtv <- # T x nr.tv.pars
+  dh_dga <- # T x nr.garch.pars
+  df_dli <-  # T x (lags to be tested)
+
+  # matrices
+  u <- g^(-1) * dg_dtv     # (1/g)*(dg/dtvpars); T x nr.tv.pars
+  x1 <- (h^(-1)) * dh_dtv  # (1/ht)*(dh/dtvpars); T x nr.tv.pars
+  x2 <- (h^(-1)) * dh_dga  # (1/ht)*(dh/dgarchpars); T x nr.garch.pars
+  r1 <- cbind(u+x1,x2)     # T x (tot.tv+garch.pars)
+  r2 <- lag0(z2,1:maxlag)  # T x maxlag
+
+  # 1 estimate tvgjrgarch, get std.residuals z=e/sqrt(gh), get SSR0 = sum(z^2-1)
+  SSR0 <- t(z2_1)%*%z2_1
+  # 2: regress z2_1 on r1~r2, get SSR
+  X <- cbind(r1,r2) # T x ncol(r1)+nocl(r2)
+  Y <- z2_1# T x 1
+  b = solve(t(X)%*%X)%*%t(X)%*%Y  # vector len=ncol(X)
+  resid = Y-X%*%t(b)   # T x 1
+  SSR1 <- t(resid)%*%resid # 1x1
+  # LM stat
+  LM <- Tobs * (SSR0-SSR1)/SSR0
+  p <- pchisq(LM,df=NCOL(r2),lower.tail=FALSE)
+
+  # ROBUST
+  # 1 as above
+  # 2 regress r2 on r1, get residual vectors
+  resid <- matrix(0,Tobs,NCOL(r2))
+  X = r1
+  for (i in 1:NCOL(r2)){
+    Y = r2[,i]
+    b = solve(t(X)%*%X)%*%t(X)%*%Y  # vector len=ncol(X)
+    resid[,i] <- Y-X%*%t(b)   # T x 1
+  }
+  # regress 1 on (z2_1)resid, get SSR
+  Y = matrix(1,Tobs,1)
+  X <- z2_1*resid
+  b = solve(t(X)%*%X)%*%t(X)%*%Y  # vector len=ncol(X)
+  resid = Y-X%*%t(b)   # T x 1
+  SSR <- t(resid)%*%resid # 1x1
+  # LM Robust
+  LMrob <- Tobs-SSR
+  prob <- pchisq(LMrob,df=NCOL(r2),lower.tail=FALSE)
+}
+
+
