149c0f3c59dc0ca1dda841dc184a0f95441d684a
gwade-integral <gwade@integral.com.au>
2026-04-15 09:59:33 +1000

    removed old code from summary() overrides

--- R/clsTVGARCH.r
+++ R/clsTVGARCH.r
@@ -232,64 +232,6 @@ setMethod("initialize","tv_class",
               print(kable(tabSummary))
               cat("\nDelta0Free: ",this@delta0free,"\n")
 
-
-              # if(!is.null(this$Estimated)){
-              #   # Calculate significance indicator for delta0
-              #   if(is.null(this$Estimated$delta0_se)) this$Estimated$delta0_se <- NaN
-              #   se <- this$Estimated$delta0_se
-              #   d0 <- this$Estimated$delta0
-              #   d0Sig <- ""
-              #   if(!is.nan(se)){
-              #     if(se*2.576 < abs(d0)) { d0Sig <- "***" }
-              #     else if(se*1.96 < abs(d0)) { d0Sig <- "** " }
-              #     else if(se*1.645 < abs(d0)) { d0Sig <- "*  " }
-              #   }
-              #   delta0Summary <- paste0(round(this$Estimated$delta0,6),"    se0 = ",round(this$Estimated$delta0_se,6),d0Sig)
-              #
-              #   # Format tv parameters if present:
-              #   parsSummary <- NULL
-              #   if(this@nr.transitions > 0){
-              #     parsVec <-  round(as.vector(this$Estimated$pars),6)
-              #     if(!is.null(this$Estimated$se) ){
-              #       seVec <- round(as.vector(this$Estimated$se),6)
-              #       seVecSig <- vector("character", length(seVec))
-              #
-              #       for(n in seq_along(parsVec)){
-              #         if(is.nan(seVec[n])) {
-              #           seVecSig[n] <- "   "
-              #         } else {
-              #           # Calculate a significance indicator
-              #           if(seVec[n]*2.576 < abs(parsVec[n]) ) { (seVecSig[n] <- "***") }
-              #           else if(seVec[n]*1.96 < abs(parsVec[n]) ) { (seVecSig[n] <- "** ") }
-              #           else if(seVec[n]*1.645 < abs(parsVec[n]) ) { (seVecSig[n] <- "*  ") }
-              #         }
-              #       }
-              #     } else {
-              #       seVec <- rep(NaN,length(this$pars))
-              #       seVecSig <- rep("   ", length(seVec))
-              #     }
-              #
-              #     seMat <- matrix(seVec,nrow=4)
-              #     colnames(seMat) <- paste("se" ,1:this@nr.transitions,sep = "")
-              #     # Build parsSummary table and insert the significance indicators
-              #     parsSummary <- data.frame(NA,stringsAsFactors = FALSE)
-              #     for (n in 1:NCOL(this$Estimated$pars)){
-              #       sig <- matrix(seVecSig[1:4],nrow=4)
-              #       parsSummary <- cbind(parsSummary,round(this$Estimated$pars[,n,drop=F],6),seMat[,n,drop=F],sig)
-              #       seVecSig <- tail(seVecSig,-4)
-              #     }
-              #   }
-              # }
-              #
-              # cat("\n\nTV OBJECT\n")
-              # cat("\nTransition Shapes: ", this$shape ,"\n")
-              # if(!is.null(this$Estimated)){
-              #   cat("\nEstimation Results:\n")
-              #   cat("\nDelta0 =",delta0Summary,"\n\n")
-              #   if(this@nr.transitions > 0) print(parsSummary[,-1])
-              #   cat("\nLog-likelihood value(TV): ",this$Estimated$value)
-              # }
-
             })
 }
 
@@ -1010,60 +952,6 @@ setMethod("summary",signature="garch_class",
             cat("\n| GARCH Estimation |  Loglik:",loglikValue)
             print(kable(tabSummary))
             cat("\nOmegaFree: ",this@omegafree,"\n")
-
-
-
-
-
-
-
-            # parsSummary <- NULL
-            # TypeNames <- c("No Garch","General","GJR Garch")
-            #
-            # if(!is.null(this$Estimated)){
-            #
-            #   parsVec <-  round(as.vector(this$Estimated$pars),6)
-            #   parsRows <- NROW(this$Estimated$pars)
-            #
-            #   if(!is.null(this$Estimated$se) ){
-            #     seVec <- round(as.vector(this$Estimated$se),6)
-            #     seVecSig <- vector("character", length(seVec))
-            #
-            #     for(n in seq_along(parsVec)){
-            #       if(is.na(seVec[n])) {
-            #         seVecSig[n] <- "   "
-            #       } else {
-            #         # Calculate a significance indicator
-            #         if(seVec[n]*2.576 < abs(parsVec[n]) ) { (seVecSig[n] <- "***") }
-            #         else if(seVec[n]*1.96 < abs(parsVec[n]) ) { (seVecSig[n] <- "** ") }
-            #         else if(seVec[n]*1.645 < abs(parsVec[n]) ) { (seVecSig[n] <- "*  ") }
-            #       }
-            #     }
-            #   } else {
-            #     seVec <- rep(NaN,length(this$pars))
-            #     seVecSig <- rep("   ", length(seVec))
-            #   }
-            #
-            #   seMat <- matrix(seVec,nrow=parsRows)
-            #   colnames(seMat) <- paste("se" ,1:max(this@order),sep = "")
-            #   # Build parsSummary table and insert the significance indicators
-            #   parsSummary <- data.frame(NA,stringsAsFactors = FALSE)
-            #   for (n in 1:NCOL(this$Estimated$pars)){
-            #     sig <- matrix(seVecSig[1:parsRows],nrow=parsRows)
-            #     parsSummary <- cbind(parsSummary,round(this$Estimated$pars[,n,drop=F],6),seMat[,n,drop=F],sig)
-            #     seVecSig <- tail(seVecSig,-parsRows)
-            #   }
-            # }
-            #
-            # cat("\nGARCH OBJECT\n")
-            # cat("\nType: ",TypeNames[this$type+1])
-            # cat("\nOrder: (",this@order[1],",",this@order[2],")")
-            # if(!is.null(this$Estimated)){
-            #   cat("\nEstimation Results:\n")
-            #   cat("\nMethod: ",this$Estimated$method,"\n")
-            #   print(parsSummary[,-1])
-            #   cat("\nLog-likelihood value(GARCH): ",this$Estimated$value)
-            # }
 
           }
   )
